f8c9a7d2a50d66e7d354cca7ee1ff5f97849e279
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-10-01 10:49:39 +0100

    Add email, age and bio fields to user details

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -13,6 +13,12 @@ const alphaError = 'must only contain letters';
 const lengthError = 'must be between 1 and 10 characters';
 
 const validateUser = [
+  body('email')
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .withMessage('Email is not formatted properly.')
+    .normalizeEmail({ all_lowercase: true }),
   body('firstName')
     .trim()
     .isAlpha()
@@ -25,21 +31,27 @@ const validateUser = [
     .withMessage(`Last name ${alphaError}.`)
     .isLength({ min: 1, max: 10 })
     .withMessage(`Last name ${lengthError}.`),
+  body('age')
+    .trim()
+    .toInt({ radix: 10 })
+    .isInt({ min: 18, max: 120, allow_leading_zeroes: false })
+    .withMessage('Age must be between 18 and 120, inclusive.'),
+  body('bio').trim().isString(),
 ];
 
 const usersCreatePost = [
   validateUser,
   (req, res) => {
-    const { firstName, lastName } = req.body;
+    const { email, firstName, lastName, age, bio } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).render('createUser', {
         title: 'Create user',
-        user: { firstName, lastName },
+        user: { email, firstName, lastName, age, bio },
         errors: errors.array(),
       });
     }
-    usersStorage.addUser({ firstName, lastName });
+    usersStorage.addUser({ email, firstName, lastName, age, bio });
     res.redirect('/');
   },
 ];
@@ -56,16 +68,16 @@ const usersUpdatePost = [
   validateUser,
   (req, res) => {
     const id = req.params.id;
-    const { firstName, lastName } = req.body;
+    const { email, firstName, lastName, age, bio } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).render('updateUser', {
         title: 'Update user',
-        user: { id, firstName, lastName },
+        user: { id, email, firstName, lastName, age, bio },
         errors: errors.array(),
       });
     }
-    usersStorage.updateUser(id, { firstName, lastName });
+    usersStorage.updateUser(id, { email, firstName, lastName, age, bio });
     res.redirect('/');
   },
 ];
--- storages/usersStorage.js
+++ storages/usersStorage.js
@@ -4,9 +4,9 @@ class UsersStorage {
     this.id = 0;
   }
 
-  addUser({ firstName, lastName }) {
+  addUser(user) {
     const id = this.id;
-    this.storage[id] = { id, firstName, lastName };
+    this.storage[id] = { id, ...user };
     this.id++;
   }
 
@@ -18,8 +18,8 @@ class UsersStorage {
     return this.storage[id];
   }
 
-  updateUser(id, { firstName, lastName }) {
-    this.storage[id] = { id, firstName, lastName };
+  updateUser(id, user) {
+    this.storage[id] = { ...this.storage[id], ...user };
   }
 
   deleteUser(id) {
